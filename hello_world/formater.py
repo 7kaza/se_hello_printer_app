6c874625202000a3e93ea23eb8f0292a44e6f446
7kaza <7kaza@usere.noreply.github.com>
2020-05-22 21:37:23 +0200

    lint

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -29,7 +29,7 @@ def format_to_json(msg, imie):
 
 def format_to_xml(msg, imie):
     return (
-        "<greetings><name>" + imie + "</name><msg>" + msg + "</msg></greetings>"
+    "<greetings><name>" + imie + "</name><msg>" + msg + "</msg></greetings>"
     )  # noqa
 
 
